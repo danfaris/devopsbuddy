6f536aaea3e1fb790c49cf748aa99ae4f262cbac
Dan Faris <danilsonsf@hotmail.com>
2018-08-13 20:19:59 +0100

    All Sorted

--- src/main/java/com/devopsbuddy/controllers/HelloWorldController.java
+++ src/main/java/com/devopsbuddy/controllers/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.devopsbuddy.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloWorldController {
